cad8fa62f4dd71ac70076967a63dad44e83e315f
vbmavani <vbmavani003@gmail.com>
2020-01-23 11:09:44 +0530

    Selection Sorting Algorithm added

---
+++ Sorting Algorithm/SelectionSort.js
@@ -0,0 +1,17 @@
+var SelectionSort=(arr,n)=>{
+  
+    for(i=0;i<n-1;i++){
+    var imin = i;
+    for (j=i+1;j<n;j++){
+      if(arr[imin]>arr[j]){
+        imin =j;
+      }
+    }
+    temp = arr[i];
+    arr[i]=arr[imin];
+    arr[imin]= temp; 
+  }
+  console.log(arr);
+  }
+  SelectionSort(arr=[19,8,6,25,3],arr.length);
+  
