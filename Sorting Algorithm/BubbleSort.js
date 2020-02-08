a6701b34aad1f4d4f966c414d27a1273a3be1f7d
vbmavani <vbmavani003@gmail.com>
2020-02-08 16:02:17 +0530

    easy Method added

--- Sorting Algorithm/BubbleSort.js
+++ Sorting Algorithm/BubbleSort.js
@@ -1,39 +1,32 @@
  //Time complexity = O(n^2)
  //best case =O(n)
- var swap = (ele1,ele2)=>{
-    temp =ele1;
-    ele1= ele2;
-    ele2 = temp; 
-    return ([ele1,ele2]);
-  }
-  
-  var bubbleSort= (arr,n)=>{
-    for(i=0;i<n-1;i++){     //we can use//for(i=0;i<n;i++) <--decrease time complexity     
-      var flag = 0;
-      for(j=0;j<n-i-1;j++){   //we can use//for(i=0;i<n;i++) <--decrease time complexity 
-        if(arr[j]>arr[j+1]){
-         [arr[j],arr[j+1]] = swap(arr[j],arr[j+1]);
-         flag = 1;
-        }
+ 
+ var bubbleSort=(arr)=>{
+  for(i=0;i<arr.length-1;i++){
+    for(j=0;j<arr.length-i-1;j++){
+      if(arr[j]>arr[j+1]){
+        [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
       }
-      if(flag == 0){return arr;}   //decrease the time complexity //don't needed 
-                                   //pass[1,2,3,4,5,6]  autamatically return array.it does't execute whole code 
-    } 
-    return arr;
+    }
   }
-  sortedarr=bubbleSort(arr=[2,4,1,3,32,1,12],arr.length);
-  console.log(sortedarr);
-  
-  
-  // sortedarr=bubbleSort(arr=[2,4,1,,11,2,1,9,12],arr.length); // [2,4,1,undefined,11,2,1,9,12]
-  //arr=sortedarr;
-  // var arr2;
-  // for(i=0;i<arr.length;i++){
-  //   if(typeof(arr[i]) == 'undefined' ){
-  //     arr2 = arr.splice(0,i);
-  //     arr.splice(0,1);
-  //     //break;
-  //   }
+  return arr;
+}
+var result = bubbleSort([1,4,2,7,8,1,1,9,12,5,1]);
+console.log(result);  
+
+  // var bubbleSort= (arr,n)=>{
+  //   for(i=0;i<n-1;i++){     //we can use//for(i=0;i<n;i++) <--decrease time complexity     
+  //     var flag = 0;
+  //     for(j=0;j<n-i-1;j++){   //we can use//for(i=0;i<n;i++) <--decrease time complexity 
+  //       if(arr[j]>arr[j+1]){
+  //        [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+  //        flag = 1;
+  //       }
+  //     }
+  //     if(flag == 0){return arr;}   //decrease the time complexity //don't needed 
+  //                                  //pass[1,2,3,4,5,6]  autamatically return array.it does't execute whole code 
+  //   } 
+  //   return arr;
   // }
-  
-  // console.log(arr,arr2)
+  // sortedarr=bubbleSort(arr=[2,4,1,3,32,1,12],arr.length);
+  // console.log(sortedarr);
--- Sorting Algorithm/SelectionSort.js
+++ Sorting Algorithm/SelectionSort.js
@@ -1,19 +1,33 @@
-//Time complexity = O(n^2)
-  
-var SelectionSort=(arr,n)=>{
-  
-    for(i=0;i<n-1;i++){
-    var imin = i;
-    for (j=i+1;j<n;j++){
-      if(arr[imin]>arr[j]){
-        imin =j;
+var selectionSort=(arr)=>{
+  for(i=0;i<arr.length;i++){
+    for(j=i+1;j<arr.length;j++){
+      if(arr[i]>arr[j]){
+        [arr[i],arr[j]] = [arr[j],arr[i]];
       }
     }
-    temp = arr[i];
-    arr[i]=arr[imin];
-    arr[imin]= temp; 
   }
-  console.log(arr);
-  }
-  SelectionSort(arr=[19,8,6,25,3],arr.length);
-  //  selectionSort(arr=['asdf','sdf','sdfr','yatsdfq','vadsfa'],arr.length)
+  return arr;
+}
+
+var result =selectionSort([6,3,7,2,4,1,9,12,2,1,10]);
+console.log(result);
+
+// //Time complexity = O(n^2)
+  
+// var SelectionSort=(arr,n)=>{
+  
+//     for(i=0;i<n-1;i++){
+//     var imin = i;
+//     for (j=i+1;j<n;j++){
+//       if(arr[imin]>arr[j]){
+//         imin =j;
+//       }
+//     }
+//     temp = arr[i];
+//     arr[i]=arr[imin];
+//     arr[imin]= temp; 
+//   }
+//   console.log(arr);
+//   }
+//   SelectionSort(arr=[19,8,6,25,3],arr.length);
+//   //  selectionSort(arr=['asdf','sdf','sdfr','yatsdfq','vadsfa'],arr.length)
