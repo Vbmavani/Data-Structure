bead8c3b87d3f7c08c7c5ea6473a9750e0c97ecd
vbmavani <vbmavani003@gmail.com>
2020-02-12 09:37:53 +0530

    some changes

--- Sorting Algorithm/SelectionSort.js
+++ Sorting Algorithm/SelectionSort.js
@@ -1,5 +1,6 @@
-var selectionSort=(arr)=>{
-  for(i=0;i<arr.length;i++){
+// //Time complexity = O(n^2)
+var selectionSort = (arr) =>{
+for(i=0;i<arr.length;i++){
     for(j=i+1;j<arr.length;j++){
       if(arr[i]>arr[j]){
         [arr[i],arr[j]] = [arr[j],arr[i]];
@@ -12,7 +13,6 @@ var selectionSort=(arr)=>{
 var result =selectionSort([6,3,7,2,4,1,9,12,2,1,10]);
 console.log(result);
 
-// //Time complexity = O(n^2)
   
 // var SelectionSort=(arr,n)=>{
   
