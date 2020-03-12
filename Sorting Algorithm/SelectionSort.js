49e9c27700a0d835a5a7f6731299e62dc399545b
vbmavani <vbmavani003@gmail.com>
2020-03-12 08:29:29 +0530

    Fix Time complexity of Selection Sort

--- Sorting Algorithm/SelectionSort.js
+++ Sorting Algorithm/SelectionSort.js
@@ -14,20 +14,3 @@ var result =selectionSort([6,3,7,2,4,1,9,12,2,1,10]);
 console.log(result);
 
   
-// var SelectionSort=(arr,n)=>{
-  
-//     for(i=0;i<n-1;i++){
-//     var imin = i;
-//     for (j=i+1;j<n;j++){
-//       if(arr[imin]>arr[j]){
-//         imin =j;
-//       }
-//     }
-//     temp = arr[i];
-//     arr[i]=arr[imin];
-//     arr[imin]= temp; 
-//   }
-//   console.log(arr);
-//   }
-//   SelectionSort(arr=[19,8,6,25,3],arr.length);
-//   //  selectionSort(arr=['asdf','sdf','sdfr','yatsdfq','vadsfa'],arr.length)
